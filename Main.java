005e299af9fdd66b77f1c9b479c7b64d02757798
DobreteVlad <58357949+DobreteVlad@users.noreply.github.com>
2019-12-01 05:24:17 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,51 +1,142 @@
 package main;
+
 import fileio.FileSystem;
-import players.*;
+import players.Player;
+import players.PlayerFactory;
+import java.io.PrintStream;
+import java.io.File;
+import java.util.ArrayList;
 
-import java.util.*;
+public final class Main {
+    private Main() {
 
-public class Main {
-
-    public static void main(String[] args) {
-        ArrayList<Player> myplayers=new ArrayList<Player>();
-        int N,M;
+    }
+    public static void main(final String[] args) {
+        ArrayList<Player> myplayers = new ArrayList<Player>();
+        int n, m;
         try {
             FileSystem fs = new FileSystem(args[0], args[1]);
-            N=fs.nextInt();
-            M=fs.nextInt();
-            String p;
-            char[][] land=new char[N][M];
-            for(int i=0;i<N;i++){
-                p=fs.nextWord();
-                for(int j=0;j<M;j++){
-                    land[i][j]=p.charAt(j);
+            n = fs.nextInt();
+            m = fs.nextInt();
+            String q;
+            char[][] land = new char[n][m];
+            for (int i = 0; i < n; i++) {
+                q = fs.nextWord();
+                for (int j = 0; j < m; j++) {
+                    land[i][j] = q.charAt(j);
                 }
             }
-            int P;
-            P = fs.nextInt();
-            for(int i = 0; i < P; i++){
+            int p;
+            p = fs.nextInt();
+            for (int i = 0; i < p; i++) {
                 String name = fs.nextWord();
                 int x = fs.nextInt();
                 int y = fs.nextInt();
-                myplayers.add(i, PlayerFactory.getInstance().CreatePlayer(name, x, y));
+                myplayers.add(i, PlayerFactory.getInstance()
+                        .createPlayer(name, x, y));
             }
-
-            int R;
-            R = fs.nextInt();
-            char[][] move_matrix= new char[R][P];
-//            System.out.println(R + "  " + P);
-//            System.out.println(fs.nextWord().charAt(0));
-//            System.out.println(fs.nextWord().charAt(0));
-
-              for (int i = 0; i < R; i++) {
+            int r;
+            r = fs.nextInt();
+            char[][] moveMatrix = new char[r][p];
+              for (int i = 0; i < r; i++) {
                   String direction = fs.nextWord();
-                  for (int j = 0; j < P; j++) {
-                      move_matrix[i][j] = direction.charAt(j);
+                  for (int j = 0; j < p; j++) {
+                      moveMatrix[i][j] = direction.charAt(j);
                   }
               }
-        }catch (Exception e1) {
+            for (int k = 0; k < r; k++) {
+                for (int l = 0; l < p; l++) {
+                        if (moveMatrix[k][l] == 'U' && !myplayers.get(l).isincapacityMove()) {
+                            myplayers.get(l).setX(myplayers.get(l).getX() - 1);
+                            moveMatrix[k][l] = '_';
+                        }
+                        if (moveMatrix[k][l] == 'D' && !myplayers.get(l).isincapacityMove()) {
+                            myplayers.get(l).setX(myplayers.get(l).getX() + 1);
+                            moveMatrix[k][l] = '_';
+                        }
+                        if (moveMatrix[k][l] == 'L' && !myplayers.get(l).isincapacityMove()) {
+                            myplayers.get(l).setY(myplayers.get(l).getY() - 1);
+                            moveMatrix[k][l] = '_';
+                        }
+                        if (moveMatrix[k][l] == 'R' && !myplayers.get(l).isincapacityMove()) {
+                            myplayers.get(l).setY(myplayers.get(l).getY() + 1);
+                            moveMatrix[k][l] = '_';
+                        }
+                        myplayers.get(l).setincapacityMove(false);
+                }
+                for (int i = 0; i < p - 1; i++) {
+                    for (int j = i + 1; j < p; j++) {
+                        if (myplayers.get(i).getX() == myplayers.get(j).getX()
+                                && myplayers.get(i).getY() == myplayers.get(j).getY()
+                                && myplayers.get(i).getHp()
+                                > 0 && myplayers.get(j).getHp() > 0 && myplayers.get(i).getName()
+                                != 'W') {
+                            myplayers.get(i).damageOvertime(myplayers.get(j), k);
+                            if (myplayers.get(i).getHp() > 0) {
+                                myplayers.get(i).firstAbility(myplayers.get(j),
+                                        land[myplayers.get(i).getX()][myplayers.get(i).getY()]);
+                                myplayers.get(i).secondAbility(myplayers.get(j),
+                                        land[myplayers.get(i).getX()][myplayers.get(i).getY()]);
+                                myplayers.get(i).totalDamage(myplayers.get(j),
+                                        land[myplayers.get(i).getX()][myplayers.get(i).getY()]);
+                                myplayers.get(j).damageOvertime(myplayers.get(i), k);
+                                myplayers.get(j).firstAbility(myplayers.get(i),
+                                        land[myplayers.get(i).getX()][myplayers.get(i).getY()]);
+                                myplayers.get(j).secondAbility(myplayers.get(i),
+                                        land[myplayers.get(i).getX()][myplayers.get(i).getY()]);
+                                myplayers.get(j).totalDamage(myplayers.get(i),
+                                        land[myplayers.get(i).getX()][myplayers.get(i).getY()]);
+                                myplayers.get(i).levelModifier(myplayers.get(j));
+                                myplayers.get(j).levelModifier(myplayers.get(i));
+                            } else {
+                                myplayers.get(j).damageOvertime(myplayers.get(i), k);
+                            }
+                        } else if (myplayers.get(i).getX() == myplayers.get(j).getX()
+                                && myplayers.get(i).getY() == myplayers.get(j).getY()
+                                && myplayers.get(i).getHp() > 0 && myplayers.get(j).getHp() > 0) {
+                            myplayers.get(j).damageOvertime(myplayers.get(i), k);
+                            if (myplayers.get(j).getHp() > 0) {
+                                myplayers.get(j).firstAbility(myplayers.get(i),
+                                        land[myplayers.get(i).getX()][myplayers.get(i).getY()]);
+                                myplayers.get(j).secondAbility(myplayers.get(i),
+                                        land[myplayers.get(i).getX()][myplayers.get(i).getY()]);
+                                myplayers.get(j).totalDamage(myplayers.get(i),
+                                        land[myplayers.get(i).getX()][myplayers.get(i).getY()]);
+                                myplayers.get(i).damageOvertime(myplayers.get(j), k);
+                                myplayers.get(i).firstAbility(myplayers.get(j),
+                                        land[myplayers.get(i).getX()][myplayers.get(i).getY()]);
+                                myplayers.get(i).secondAbility(myplayers.get(j),
+                                        land[myplayers.get(i).getX()][myplayers.get(i).getY()]);
+                                myplayers.get(i).totalDamage(myplayers.get(j),
+                                        land[myplayers.get(i).getX()][myplayers.get(i).getY()]);
+                                myplayers.get(j).levelModifier(myplayers.get(i));
+                                myplayers.get(i).levelModifier(myplayers.get(j));
+                            } else {
+                                myplayers.get(i).damageOvertime(myplayers.get(j), k);
+                            }
+                        }
+
+                    }
+                }
+            }
+            try {
+                PrintStream o = new PrintStream(new File(args[1]));
+                System.setOut(o);
+                for (int i = 0; i < p; i++) {
+                    if (myplayers.get(i).getHp() <= 0) {
+                        System.out.println(myplayers.get(i).getName() + " " + "dead");
+                    } else {
+                        System.out.println(myplayers.get(i).getName() + " "
+                                + myplayers.get(i).getLevel() + " " + myplayers.get(i).getXp()
+                                + " " + myplayers.get(i).getHp() + " " + myplayers.get(i).getX()
+                                + " " + myplayers.get(i).getY());
+                    }
+                }
+            } catch (Exception var8) {
+                var8.printStackTrace();
+            }
+        } catch (Exception e1) {
             e1.printStackTrace();
         }
-
     }
 }
